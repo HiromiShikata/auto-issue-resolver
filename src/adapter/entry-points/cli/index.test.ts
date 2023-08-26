02e9233b56628f1cc05a6211b43ad606a277e4ee
HiromiShikata <shikata.hiromi@gmail.com>
2023-08-26 15:38:25 +0000

    feat: create monitor command

--- src/adapter/entry-points/cli/index.test.ts
+++ src/adapter/entry-points/cli/index.test.ts
@@ -1,29 +1,43 @@
 import { execSync } from 'child_process';
 
 describe('commander program', () => {
-  it('should success', () => {
-    const output = execSync(
-      'npx ts-node ./src/adapter/entry-points/cli/index.ts umino-bot HiromiShikata/auto-issue-resolver#18 -t 10',
-    ).toString();
+  // describe('monitor', () => {
+  //   it('should success', () => {
+  //     const output = execSync(
+  //       'npx ts-node ./src/adapter/entry-points/cli/index.ts bot-test-hiromishikata -t 10 -m 1',
+  //     ).toString();
+  //
+  //     expect(output.trim()).toEqual(JSON.stringify({}));
+  //   });
+  // });
+  // describe('resolve', () => {
+  //   it('should success', () => {
+  //     const output = execSync(
+  //       'npx ts-node ./src/adapter/entry-points/cli/index.ts bot-test-hiromishikata -i HiromiShikata/auto-issue-resolver#18 -t 10',
+  //     ).toString();
+  //
+  //     expect(output.trim()).toEqual(JSON.stringify({}));
+  //   });
+  // });
+  describe('help', () => {
+    it('should show help', () => {
+      const output = execSync(
+        'npx ts-node ./src/adapter/entry-points/cli/index.ts -h',
+      ).toString();
 
-    expect(output.trim()).toEqual(JSON.stringify({}));
-  });
-  it('should show help', () => {
-    const output = execSync(
-      'npx ts-node ./src/adapter/entry-points/cli/index.ts -h',
-    ).toString();
-
-    expect(output.trim()).toEqual(
-      `Usage: Auto Issue Resolver [options] <botUserName> <issueId>
+      expect(output.trim())
+        .toEqual(`Usage: Auto Issue Resolver [options] <botUserName>
 
 Arguments:
-  botUserName                 bot user name of github
-  issueId                     owner/repo#issue_number
+  botUserName                         bot user name of github
 
 Options:
-  -t, --try-count <tryCount>  try count
-  -h, --help                  display help for command
-`.trim(),
-    );
+  -t, --try-count <tryCount>          try count
+  -m, --monitor-hours <monitorHours>  The number of hours the system will
+                                      continuously monitor or observe for
+                                      changes or updates.
+  -i, --issue-id <issueId>            owner/repo#issue_number
+  -h, --help                          display help for command`);
+    });
   });
 });
